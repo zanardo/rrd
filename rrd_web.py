9b3ced9c2b8ba0367ce25b98c9c496c6cb7362f2
Zanardo <zanardo@gmail.com>
2011-08-09 21:09:31 -0300

    rrd_web.py: creating route list-graph-all-periods

--- rrd_web.py
+++ rrd_web.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright (c) J. A. Zanardo Jr. <zanardo@gmail.com>
 # Please refer to COPYING for license details
 
@@ -7,12 +8,16 @@ import os.path
 import tempfile
 import bottle
 import rrdtool
+import time
 
 __VERSION__ = '0.3'
 
 app = bottle.Bottle()
 
 DIR = os.path.realpath(os.path.curdir)
+
+PERIODS = [ ('Horário', 7200), ('Diário', 86400), ('Semanal', 604800),
+           ('Mensal', 1814400), ('Anual', 21772800) ]
 
 @app.route('/render-graph/:name#[a-z0-9\-]+#/:width#\d+#/:height#\d+#/:offset#\d+#')
 def render_graph(name, width, height, offset):
@@ -62,6 +67,49 @@ def render_graph(name, width, height, offset):
 
     bottle.response.headers['Content-Type'] = 'image/png'
     return img
+
+@app.route('/list-graph-all-periods/:name#[a-z0-9\-]+#')
+def list_graph_all_periods(name):
+
+    template = """
+        <header>
+            <link rel="stylesheet" type="text/css" href="/styles.css">
+            <title>Gráficos RRD - {{ name }}</title>
+        </header>
+        <h1>Gráficos RRD - {{ name }}</h1>
+        %for period in periods:
+            <h3>{{ period[0] }}</h3>
+            <img src="/render-graph/{{ name }}/650/200/{{ period[1] }}">
+        %end
+        {{!footer}}
+    """
+
+    return bottle.template(template, name=name, periods=PERIODS,
+                           footer=page_footer())
+
+@app.route('/styles.css')
+def css():
+    bottle.response.content_type = 'text/css'
+    return """
+        body { margin: 1em 2em 2em 2em; }
+        h1, h2, h3, h4, h5, h6 {
+          font-family: "Gill Sans", "Trebuchet MS", Verdana, sans-serif;
+          font-weight: normal; 
+        }
+        img { border: none; }
+        dt { font-weight: bold; }
+        pre { background-color: #C0C0C0; font-weight: bold; }
+        a:link { color: #0000EE; }
+        a:visited { color: #0000EE; }
+        a:active { color: #0000EE; }
+        a:hover { color: #0000EE; }
+    """
+
+def page_footer():
+    return """<p><small><i>Página gerada em %s por 
+        <a href="http://zanardo.org/rrd.html">rrd %s</a></i></small>""" % \
+            ( time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()),
+             __VERSION__ )
 
 if __name__ == '__main__':
     bottle.debug(True)
